55aa740d6e79f9d1c152dd1919ed79901923c865
basantpatidar <basantpatidar33@gmail.com>
2020-05-10 22:18:13 -0400

    reducer

---
+++ src/reducer.js
@@ -0,0 +1,37 @@
+import * as actions from './actionTypes';
+
+let lastId = 0;
+
+export default function reducer(state = [], action){
+    switch(action.type){
+        case actions.BUG_ADDED:
+            return [
+                ...state, {
+                    id: ++lastId,
+                    description: action.payload.description,
+                    resolved: false
+                }
+            ];
+        case actions.BUG_REMOVED: 
+            return state.filter(bug => bug.id !== action.payload.id);
+
+        default:
+            console.log("This is Default");
+            return state;
+    }
+
+
+
+    // if(action.type === 'bugAdded')
+    // return [
+    //     ...State, {
+    //         id: ++lastId,
+    //         description: action.payload.description,
+    //         resolved: false
+    //     }
+    // ]; 
+    // else if (action.type === 'bugRemoved'){
+    //     return State.filter(bug => bug.id !== action.payload);
+    // }
+    // return State;
+}
